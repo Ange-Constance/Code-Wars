8e8081fb31d4d25e90ce84af1814df18f77984a0
Ange-Constance <constance.nimuhire@gmail.com>
2024-03-20 11:17:17 +0200

    remove exclamation marks

---
+++ Let_and_Var/removeExclamationMarks.js
@@ -0,0 +1,7 @@
+function removeExclamationMarks(s) {
+    return s.replace(/!/g, '');
+  }
+  
+  const originalString = "Hello! World!";
+  const stringWithoutExclamationMarks = removeExclamationMarks(originalString);
+  console.log(stringWithoutExclamationMarks); 
